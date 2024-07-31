eb1ad527c1d7d5f946943a8590396b63e9f06e5a
Victor Cortés <cortes.victorhugo@gmail.com>
2024-07-31 00:26:28 -0500

    test: configuring libraries and mocks for unit tests

---
+++ src/mocks/data.ts
@@ -0,0 +1,27 @@
+export const ITEMS = [
+  {
+    id: 1,
+    userId: 1,
+    title: 'The Great Gatsby',
+    body: 'The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald.',
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: 'The Catcher in the Rye',
+    body: 'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951.',
+  },
+  {
+    id: 3,
+    userId: 2,
+    title: 'To Kill a Mockingbird',
+    body: 'To Kill a Mockingbird is a novel by Harper Lee published in 1960.',
+  },
+]
+
+export const ITEM = {
+  id: 4,
+  userId: 2,
+  title: '1984',
+  body: '1984 is a dystopian social science fiction novel by English novelist George Orwell.',
+}
